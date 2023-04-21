287be0f3b150740579f8369867191853269ae525
mikayla-eckelcifrese <mikayla.eckelcifrese@students.mccc.edu>
2023-04-20 21:40:05 -0400

    Project 4

--- H_angular_distribution.py
+++ H_angular_distribution.py
@@ -11,69 +11,68 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 import numpy as np
+from numpy import vectorize
 
-#import ipyvolume as ipv
+# import ipyvolume as ipv
 
 # Import special functions 
 import scipy.special as spe
 
+@vectorize
+def psi_ang(phi, theta, l=0, m=0):
+    sphHarm = spe.sph_harm(m, l, phi, theta)  # sph_harm switches theta and phi
 
+    if m < 0 and (abs(m) % 2) == 0:
+        return np.sqrt(2) * sphHarm.imag
+    elif m < 0:
+        return np.sqrt(2) * (-1) * sphHarm.imag
+    elif m > 0 and (m % 2) == 0:
+        return np.sqrt(2) * sphHarm.real
+    elif m > 0:
+        return np.sqrt(2) * (-1) * sphHarm.real
 
-def psi_ang(phi,theta,l=0,m=0):
-    
-    sphHarm = spe.sph_harm(m,l,phi,theta) # sph_harm switches theta and phi
-    
-    if (m < 0 and (abs(m) % 2) == 0):
-       return np.sqrt(2)*sphHarm.imag
-    elif (m < 0):
-        return np.sqrt(2)*(-1)*sphHarm.imag
-    elif (m > 0 and (m % 2) == 0):
-       return np.sqrt(2)*sphHarm.real
-    elif (m > 0):
-       return np.sqrt(2)*(-1)*sphHarm.real
-   
     return sphHarm.real
 
 
-phi, theta = np.linspace(0, 2*np.pi, 100), np.linspace(0, np.pi, 100)
+phi, theta = np.linspace(0, 2 * np.pi, 100), np.linspace(0, np.pi, 100)
 
 phi, theta = np.meshgrid(phi, theta)
 
-Ylm = psi_ang(phi,theta,l=0,m=0)
+@vectorize
+def plot_angular_dist(l, m):
+    Ylm = psi_ang(phi, theta, l=l, m=m)
 
-x = np.sin(theta) * np.cos(phi) * abs(Ylm)
-y = np.sin(theta) * np.sin(phi) * abs(Ylm)
-z = np.cos(theta) * abs(Ylm)
+    x = np.sin(theta) * np.cos(phi) * abs(Ylm)
+    y = np.sin(theta) * np.sin(phi) * abs(Ylm)
+    z = np.cos(theta) * abs(Ylm)
 
-'''Set up the 3D Canvas'''
+    '''Set up the 3D Canvas'''
 
-fig = plt.figure(figsize=(10,10))
-ax = fig.add_subplot(111, projection='3d')
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
 
-''' Normalize color bar to [0,1] scale'''
+    ''' Normalize color bar to [0,1] scale'''
 
-fcolors = (Ylm - Ylm.min())/(Ylm.max() - Ylm.min())
+    fcolors = (Ylm - Ylm.min()) / (Ylm.max() - Ylm.min())
 
-'''Make 3D plot of real part of spherical harmonic'''
+    '''Make 3D plot of real part of spherical harmonic'''
 
-ax.plot_surface(x, y, z, facecolors=cm.seismic(fcolors), alpha=0.3)
+    ax.plot_surface(x, y, z, facecolors=cm.seismic(fcolors), alpha=0.3)
 
+    ''' Project 3D plot onto 2D planes'''
 
-''' Project 3D plot onto 2D planes'''
+    cset = ax.contour(x, y, z, 20, zdir='z', offset=-1, cmap='summer')
+    cset = ax.contour(x, y, z, 20, zdir='y', offset=1, cmap='winter')
+    cset = ax.contour(x, y, z, 20, zdir='x', offset=-1, cmap='autumn')
 
-cset = ax.contour(x, y, z,20, zdir='z',offset = -1, cmap='summer')
-cset = ax.contour(x, y, z,20, zdir='y',offset =  1, cmap='winter' )
-cset = ax.contour(x, y, z,20, zdir='x',offset = -1, cmap='autumn')
+    ''' Set axes limit to keep aspect ratio 1:1:1 '''
 
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_zlim(-1, 1)
 
-''' Set axes limit to keep aspect ratio 1:1:1 '''
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
-ax.set_xlim(-1, 1)
-ax.set_ylim(-1, 1)
-ax.set_zlim(-1, 1)
-
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
-plt.show()
+    plt.show()
---
+++ H_radial_distribution.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 12:45:37 2023
+
+@author: mgoryll
+
+Based on the code from https://dpotoyan.github.io/Chem324/H-atom-wavef.html 
+"""
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+from numpy import vectorize
+
+# Import special functions 
+import scipy.special as spe
+
+def psi_R(r, n=1, l=0):
+    coeff = np.sqrt((2.0 / n) ** 3 * spe.factorial(n - l - 1) / (2.0 * n * spe.factorial(n + l)))
+
+    laguerre = spe.assoc_laguerre(2.0 * r / n, n - l - 1, 2 * l + 1)
+
+    return coeff * np.exp(-r / n) * (2.0 * r / n) ** l * laguerre
+
+
+def plot_rad_dist(n, l):
+    r = np.linspace(0, 100, 1000)
+
+    R = psi_R(r, n=n, l=l)
+
+    plot = plt.plot(r, r ** 2 * R ** 2, lw=3, label=f"n={n}")
+
+    plt.xlabel('$r [a_0]$', fontsize=12)
+
+    plt.ylabel('$r^2 R^2_{nl}(r)$', fontsize=12)
+
+    plt.grid('True')
+
+    plt.legend()
+    # plt.show()
+    return plot
